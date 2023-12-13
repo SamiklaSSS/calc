0734df17086faa2bfb71dde00591b058a669b64d
SamiklaSSS <149561953+SamiklaSSS@users.noreply.github.com>
2023-12-13 10:16:52 +0200

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,24 +1,43 @@
 
 # konvertē datu mērvienības
 def megabit_to_megabyte(megabit):
-   result = 0
+   result = (megabit)*8
    return result
+
+
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
+   #if(kilometers) = 100{
+   # result = (litres)
+   #} else {
+   #    d = (litres,kilometers)/(litres,kilometers)*100
+   #    result = d
+   # }
+
    return result
+
+
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = (celsius)*1.8+32
     return result
+
+
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
-    result = 0
+    #(tail) = i
+    #for i + (i-1) = (sigma)
+    #(tail)!==0
+    #result = (sigma)
     return result
+
+
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
 def weight(grams):
+    #if 100<(grams):
+
     return str(grams) + "g"
